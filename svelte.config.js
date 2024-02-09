49cf06708a774e7f6120ea7d48460eaafd820f71
Ronald de Lange <ronalddelange@MacBook-Pro-van-Ronald.local>
2024-02-09 17:10:52 +0100

    first commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// svelte.config.js
+
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+ kit: {
+  adapter: adapter()
+ },
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+ vitePlugin: {
+  experimental: {
+   // Allows you to hold ctrl+shift and click on an item in the browser and it then
+   // opens that components location in VSCode
+   inspector: {
+    holdMode: true
+   }
+  }
+ }
 };
 
 export default config;
