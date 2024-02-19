bf9f1e201ad3fa1062c22cecddc21e591180d476
Ronald de Lange <ronalddelange@MBP-van-Ronald.lan>
2024-02-20 00:36:56 +0100

    addlog cross origin

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,13 +9,23 @@ const securityHeaders = {
 }
 
 
+export const corsHeaders = {
+	'Access-Control-Allow-Credentials': 'true',
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'OPTIONS,POST',
+	'Access-Control-Allow-Headers':
+		'authorization, x-client-info, apikey, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+};
 
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const response = await resolve(event);
-    Object.entries(securityHeaders).forEach(
-        ([header, value]) => response.headers.set(header, value)
-    );
+	if (event.request.method !== 'OPTIONS') {
+		const response = await resolve(event);
+		for (const [key, value] of Object.entries(corsHeaders)) {
+			response.headers.set(key, value);
+		}
+		return response;
+	}
 
-    return response;
-}
+	return new Response('ok', { headers: corsHeaders });
+};
