7995c349ac76628fcce5a27309b4c7341132f508
Ronald de Lange <ronalddelange@MBP-van-Ronald.lan>
2024-02-19 22:22:26 +0100

    addlog cross origin

--- src/routes/start/addLog/+server.ts
+++ src/routes/start/addLog/+server.ts
@@ -5,7 +5,7 @@ import {addLog} from '$lib/server/db';
 export async function POST({ request }) { 
     const data = await request.json();
     addLog(data.uniqueid,data.lastrequest,data.lastreset)
-    const response = new Response(JSON.stringify({"succes":"yes"}));
+    const response = new Response(JSON.stringify({"succes":true}));
     response.headers.append('Access-Control-Allow-Origin', '*');
     return response;
 } 
